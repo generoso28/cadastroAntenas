0fcae8b5478026693b5435f8b35cd40b48a57ad6
generoso28 <131410501+generoso28@users.noreply.github.com>
2025-05-26 20:30:57 -0300

    Added support to NIO

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,26 @@
+import br.inatel.cdg.antenas.Antena;
+import br.inatel.cdg.antenas.Antena4g;
+import br.inatel.cdg.antenas.Antena5g;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.print("Hello, world!");
+        // Exemplo de uso das classes Antena4g e Antena5g
+        Antena4g antena4g = new Antena4g(1, 100, 200, 1800);
+        System.out.println(antena4g.getTipo());
+        antena4g.configurarFrequencia(1800); // Frequência válida para 4G
+
+        Antena5g antena5g = new Antena5g(2, 300, 400, 3500);
+        System.out.println(antena5g.getTipo());
+        antena5g.configurarFrequencia(3500); // Frequência válida para 5G
+
+        antena5g.save();
+        antena4g.save();
+        Antena.read();
+
+
+
+
+
+
     }
 }
--- src/br/inatel/cdg/antenas/Antena.java
+++ src/br/inatel/cdg/antenas/Antena.java
@@ -2,6 +2,13 @@ package br.inatel.cdg.antenas;
 
 import br.inatel.cdg.interfaces.Configuravel;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardOpenOption;
+import java.util.List;
+
 public abstract class Antena implements Configuravel {
     protected int id;
     protected long localizacaox;
@@ -15,8 +22,41 @@ public abstract class Antena implements Configuravel {
         this.frequencia = frequencia;
     }
     public abstract String getTipo();
-    public double getFrequencia(){
-        return frequencia;
+
+    @Override
+    public String toString() {
+        return getTipo() + "," + id + "," + localizacaox + "," + localizacaoy + "," + frequencia;
+    }
+    public void save(){
+        Path arquivo = Paths.get("antenas.txt");
+        try{
+            Files.writeString(arquivo, this + System.lineSeparator(),
+                    StandardOpenOption.CREATE, StandardOpenOption.APPEND);
+        }catch (IOException e){
+            System.out.println("Erro ao salvar a antena: " + e.getMessage());
+        }
+    }
+    public static void read(){
+        Path arquivo = Paths.get("antenas.txt");
+        try{
+            List<String> antenas = Files.readAllLines(arquivo);
+            System.out.println("Antenas salvas:");
+            for (String antena : antenas) {
+                String[] partes = antena.split(",");
+                if (partes.length == 5) {
+                    String tipo = partes[0];
+                    int id = Integer.parseInt(partes[1]);
+                    long localizacaox = Long.parseLong(partes[2]);
+                    long localizacaoy = Long.parseLong(partes[3]);
+                    double frequencia = Double.parseDouble(partes[4]);
+                    System.out.println("Tipo: " + tipo + ", ID: " + id + ", Localização: (" + localizacaox + ", " + localizacaoy + "), Frequência: " + frequencia);
+                } else {
+                    System.out.println("Formato inválido na linha: " + antena);
+                }
+            }
+        }catch (IOException e){
+            System.out.println("Erro ao ler o arquivo: " + e.getMessage());
+        }
     }
 
 }
